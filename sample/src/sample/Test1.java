61d28278e474d65f6736366b42b80669c966ca1d
Yogi1519 <YOGESH@LAPTOP-HG4EAF1R>
2020-07-15 20:04:28 +0530

    first commit

---
+++ sample/src/sample/Test1.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//B1 x= new C1(); //calling constructor
+		
+		A1 y=new C1();
+		y.sound();
+		
+		
+		
+	}
+
+}
